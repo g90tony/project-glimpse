4c93e699daa0fbc44fae590168470a70dde8ac06
Caleb Tony <calebmbugua@gmail.com>
2021-05-31 05:40:53 +0300

    added profile and project models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Models):
+    display_picture = CloudinaryField('image', default=None)
+    user_bio = models.TextField()
+    user_mail = models.EmailField()
+
+
+class Project(models.Model):
+    profile = models.ForeignKey(Profile)
+    title = models.CharField(max_length=255)
+    landing_page = CloudinaryField('image', default=None)
+    detailed_description = models.TextField()
+    link = models.CharField()
