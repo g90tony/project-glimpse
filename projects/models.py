da71b0c791fa00e6d39f390a3a925212877e91d7
Caleb Tony <calebmbugua@gmail.com>
2021-05-31 05:56:37 +0300

    minor debugging fixes

--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
+
 # Create your models here.
-class Profile(models.Models):
+class Profile(models.Model):
     display_picture = CloudinaryField('image', default=None)
     user_bio = models.TextField()
     user_mail = models.EmailField()
 
 
 class Project(models.Model):
-    profile = models.ForeignKey(Profile)
-    title = models.CharField(max_length=255)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
     landing_page = CloudinaryField('image', default=None)
     detailed_description = models.TextField()
-    link = models.CharField()
+    link = models.CharField(max_length=225)
