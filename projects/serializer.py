0fd335fd899b4b3a4839e71daad86667f99605bd
Caleb Tony <calebmbugua@gmail.com>
2021-05-31 10:57:24 +0300

    renamed productaSerializer to ProjectSerializer

--- projects/serializer.py
+++ projects/serializer.py
@@ -2,7 +2,7 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import Profile, Project
 
-class ProductionSerializer(serializers.ModelSerializer):
+class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields =("profile", "title", "landing_page", "detailed_description", "link",)
