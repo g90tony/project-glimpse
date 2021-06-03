68ee349c03c412a3e73e96282aa00dc812751349
Caleb Tony <calebmbugua@gmail.com>
2021-06-03 13:03:08 +0300

    added api and view routes

--- projects/url.py
+++ projects/url.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 
 from .import views
@@ -6,8 +7,15 @@ from .import views
 app_name = 'projects'
 
 urlpatterns = [
-    path(r'^api/projects/list/all$', views.index.as_view()),
-    path(r'^api/projects/view/(?P<project_id>[0-9])$', views.view_project.as_view()),
-    path(r'^api/profile/)$', views.user_profile.as_view()),
-    path(r'^api/profile/view/(?P<profile_id>[0-9])$', views.view_profile.as_view()),
+    path('api/project/', views.ListProjects.as_view()),
+    path('api/project/(?P<project_id>[0-9])', views.SingleProject.as_view()),
+    
+    path('api/profile/(?P<project_id>[0-9])', views.SingleProfile.as_view()),
+  
+    path('', views.index, name='Home'),
+    path('profile/', views.profile, name='Profile'),  
+    path('profile/create', views.create_profile, name='Create Profile'),
+    
+    path('project/new', views.upload, name='New Project'),  
+    path('project/view', views.view, name='View Project'),  
 ]
